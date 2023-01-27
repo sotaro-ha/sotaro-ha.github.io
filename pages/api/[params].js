05604f15a272204b00d89af8c078009619764a3e
横井総太朗 <alphacomplex.104729@gmail.com>
2023-01-28 02:00:00 +0900

    specify URL

--- pages/api/[params].js
+++ pages/api/[params].js
@@ -4,7 +4,7 @@ import path from 'path';
 let data = [];
 let name = [];
 for (let i = 0; i < 2; i++) {
-  const file = path.join(process.cwd(), "/data_" + i + ".txt");
+  const file = path.join(process.cwd(), "/var/task/.next/server/public/data_" + i + ".txt");
   data = data.concat(fs.readFileSync(file, "utf-8").split("\n"));
 }
 function refineData(data) {
